923b4bcabe75a6916a89e00159cdc20100d425b5
nolramph <marlon.peralta17@gmail.com>
2020-11-05 22:50:18 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * Sandugo Template
+ * 
+ * @package Sandugo
+ */
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Wordpress Theme | Sandugo</title>
+ </head>
+ <body>
+     Hello Wordpress!
+ </body>
+ </html>
